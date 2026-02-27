fd0428e5727b39cfc7c88ae9094a7d22af89cb08
Deepanshu-Z <deepanshupokhriyal07@gmail.com>
2026-02-27 16:15:39 +0530

    Enrolled courses corrected return type

--- lib/courses/getEnrolledCourses.ts
+++ lib/courses/getEnrolledCourses.ts
@@ -5,5 +5,5 @@ export default async function getEnrolledCourses(id: string) {
   const response = await fetch(
     `${BASE_URL}/api/enroll/enroll-course/${id}`,
   ).then((res) => res.json());
-  return response;
+  return response.data.courses;
 }
