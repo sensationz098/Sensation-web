368431b2e8a62852e77354bc0af36e28077ee39c
Deepanshu-Z <deepanshupokhriyal07@gmail.com>
2026-03-03 15:25:24 +0530

    Getting order_id from backend by sending packet

--- app/(welcome)/welcome/course/[id]/action/paymentLogic.ts
+++ app/(welcome)/welcome/course/[id]/action/paymentLogic.ts
@@ -1,7 +1,25 @@
 import getUserProfileDetails from "@/lib/user/getUserProfileDetails";
 import { describe } from "zod/v4/core";
 import calculateEndDate from "./calculateEndDate";
-
+import createOrder from "@/lib/payment/createOrder";
+export interface DataType {
+  contact: string;
+  description: string;
+  email: string;
+  profile_id: string;
+  course_id: string;
+  start_date: string;
+  end_date: string;
+  schedule_id: string;
+  name: string;
+  teacher_id: string;
+  base_amount: number;
+  gst_percentage: number;
+  currency: string;
+  discount_id?: string | null;
+  discount_amount: number;
+  counsellor_id?: string | null;
+}
 const paymentLogic = async ({
   id,
   course,
@@ -15,25 +33,30 @@ const paymentLogic = async ({
   basePrice,
 }: any) => {
   const response = await getUserProfileDetails(id);
-  const end_date = calculateEndDate(startDate, currentPlan.duration);
-  const data = {
+  const end_date = calculateEndDate(
+    startDate,
+    currentPlan.duration,
+  ).toISOString();
+  console.log("DATE ARE: ", startDate, end_date);
+  const data: DataType = {
     contact: response.contact,
     description: course.description,
     email: response.email,
     profile_id: response.id,
     course_id: course.id,
-    start_date: startDate,
+    start_date: new Date(startDate).toISOString(),
     end_date: end_date,
     schedule_id: selectedSchedule.id,
     name: response.full_name,
     teacher_id: selectedTeacher.id,
     base_amount: basePrice,
-    gst_percenage: response.gst,
-    currency: response.currency,
+    gst_percentage: Number(course.gst),
+    currency: course.currency,
     discount_id: discountId,
     discount_amount: appliedDiscount,
     counsellor_id: counsellorId,
   };
+  const result = await createOrder(data);
   console.log("SENDING PACKET: ", data);
 };
 export default paymentLogic;
--- lib/payment/createOrder.ts
+++ lib/payment/createOrder.ts
@@ -1,5 +1,14 @@
+import { DataType } from "@/app/(welcome)/welcome/course/[id]/action/paymentLogic";
 import { BASE_URL } from "@/config/api";
 
-export default async function createOrder() {
-  const response = await fetch(`${BASE_URL}/api/payment/create-order`, {});
+export default async function createOrder(data: DataType) {
+  const response = await fetch(`${BASE_URL}/api/payment/create-order`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+    next: { revalidate: 0 },
+  }).then((res) => res.json());
+  console.log(response.data);
 }
